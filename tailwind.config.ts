48bb6749fa9c01aa9ad8c97c9f96c4cfb536fd61
Matthijs de Keijzer <dekeijzer.matthijs@proton.me>
2025-10-11 14:02:43 +0200

    fix: remove android studio dependencies

---
+++ tailwind.config.ts
@@ -0,0 +1,12 @@
+import type { Config } from 'tailwindcss';
+
+const config: Config = {
+  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+  darkMode: ['class', '[data-theme="dark"]'],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
+
+export default config;
